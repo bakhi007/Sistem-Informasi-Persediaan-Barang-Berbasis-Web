6af3d70c25a052d56f1306da986e581a4f91e4ce
bakhi007 <lightred715@gmail.com>
2024-09-25 12:40:50 +0700

    add: description

--- resources/views/dashboard/transaksi/sale/index.blade.php
+++ resources/views/dashboard/transaksi/sale/index.blade.php
@@ -1,4 +1,4 @@
 <x-sidebar>
 <x-slot:title> {{ $title }} </x-slot:title> 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis et dolore cumque beatae! Ex ducimus asperiores illum incidunt exercitationem, eaque saepe cupiditate vero rerum, debitis sit facere vitae, officia possimus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis et dolore cumque beatae! Ex ducimus asperiores illum incidunt exercitationem, eaque saepe cupiditate vero rerum, debitis sit facere vitae, officia possimus. hj</p>
 </x-sidebar>
